75d41eaf4c025c886467ca9d0e87f36a22a77945
Echidna <176174870+EchidnaTheG@users.noreply.github.com>
2025-07-18 05:16:37 -0300

    Added Help and Exit Commands

--- main.go
+++ main.go
@@ -7,6 +7,49 @@ import (
 	"strings"
 )
 
+type CliCommand struct{
+	name string
+	description string
+	callback func() error
+}
+
+var SupportedCommands map[string]CliCommand =  make(map[string]CliCommand)
+
+
+func Help() error {
+		fmt.Println("Welcome to the Pokedex!")
+		fmt.Println("Usage:")
+		fmt.Println("\nhelp: Displays a help message")
+		fmt.Println("exit: Exit the Pokedex\n")
+		return nil
+	}
+
+func Exit() error{
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	defer os.Exit(0)
+	return nil
+}
+
+
+func init(){
+	SupportedCommands=  make(map[string]CliCommand)
+	SupportedCommands["help"] =CliCommand{
+	name: "help",
+	description: "Gives Help about app",
+	callback: Help,
+	}
+	SupportedCommands["exit"]=CliCommand{
+		name: "exit",
+		description:"exits the program",
+		callback: Exit,
+	}
+
+	
+} 
+
+
+
+
 func cleanInput(text string) []string {
 	return strings.Fields(text)
 
@@ -14,13 +57,21 @@ func cleanInput(text string) []string {
 
 func main() {
 	Scanner := bufio.NewScanner(os.Stdin)
+	
 	for {
 		fmt.Print("Pokedex > ")
 		Scanner.Scan()
-		userInputs := cleanInput(Scanner.Text())
+		userInputs := cleanInput(strings.ToLower(Scanner.Text()))
 		
 		if len(userInputs) > 0{
-		fmt.Printf("Your command was: %v\n", strings.ToLower(userInputs[0]))
+			_,ok := SupportedCommands[userInputs[0]]; if !ok{
+				println("Unknown Command")
+				continue
+			}
+			err := SupportedCommands[userInputs[0]].callback()
+			if err != nil{
+				fmt.Printf("Error!: %v\n", err)
+			}
 		}
 	}
 }
--- main_test.go
+++ main_test.go
@@ -36,4 +36,3 @@ func TestCleanInput(t *testing.T){
 	} 
 
 }
-
