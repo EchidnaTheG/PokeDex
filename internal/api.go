b383ea73788fa374accc0cb3fd24a2eafceaf076
Echidna <176174870+EchidnaTheG@users.noreply.github.com>
2025-07-24 00:37:00 -0300

    Added Initial Support For Explore Command, rewrote initial CLI Logic to make it functional, fixed some bugs such as not reading http response codes, api.go file will probably benefit from moving  the json structs  to a models.go file

--- internal/api.go
+++ internal/api.go
@@ -13,6 +13,56 @@ import (
 const API string = "https://pokeapi.co/api/v2"
 
 
+type LocationAreaData struct {
+    ID                   int                    `json:"id"`
+    Name                 string                 `json:"name"`
+    GameIndex            int                    `json:"game_index"`
+    EncounterMethodRates []EncounterMethodRate  `json:"encounter_method_rates"`
+    Location             LocationResult                `json:"location"`
+    Names                []AreaName             `json:"names"`
+    PokemonEncounters    []PokemonEncounter     `json:"pokemon_encounters"`
+}
+
+// Encounter method rate structure
+type EncounterMethodRate struct {
+    EncounterMethod LocationResult               `json:"encounter_method"`
+    VersionDetails  []MethodVersionDetail `json:"version_details"`
+}
+
+// Version details for encounter methods
+type MethodVersionDetail struct {
+    Rate    int     `json:"rate"`
+    Version LocationResult `json:"version"`
+}
+
+// Area name structure
+type AreaName struct {
+    Name     string  `json:"name"`
+    Language LocationResult `json:"language"`
+}
+
+// Pokemon encounter structure
+type PokemonEncounter struct {
+    Pokemon        LocationResult                      `json:"pokemon"`
+    VersionDetails []PokemonEncounterVersionDetail `json:"version_details"`
+}
+
+// Version details for pokemon encounters
+type PokemonEncounterVersionDetail struct {
+    Version          LocationResult            `json:"version"`
+    MaxChance        int                `json:"max_chance"`
+    EncounterDetails []EncounterDetail  `json:"encounter_details"`
+}
+
+// Individual encounter detail
+type EncounterDetail struct {
+    MinLevel        int       `json:"min_level"`
+    MaxLevel        int       `json:"max_level"`
+    ConditionValues []LocationResult `json:"condition_values"`
+    Chance          int       `json:"chance"`
+    Method          LocationResult   `json:"method"`
+}
+
 // Individual location result
 type LocationResult struct {
     Name string `json:"name"`
@@ -76,6 +126,9 @@ func GetLocationData(configPointer *Config, isMapb bool, CACHE *Cache ) (Results
 	if err != nil{
 		return []LocationResult{}, fmt.Errorf("connection Error: %v", err)
 	}
+	if res.StatusCode != http.StatusOK{
+		return []LocationResult{}, fmt.Errorf("Bad Request: %v\n", res.StatusCode)
+	}
 	defer res.Body.Close()
 	cacheValue, err := io.ReadAll(res.Body)
     if err != nil {
@@ -95,4 +148,48 @@ func GetLocationData(configPointer *Config, isMapb bool, CACHE *Cache ) (Results
 	var LocationResult []LocationResult
 	LocationResult = append(LocationResult, NewLocationData.Results...)
 	return LocationResult, nil
+}
+
+func GetPokemonInArea(area string, CACHE *Cache) (Pokemon []PokemonEncounter, err error){
+	
+	FullPath := API + "/location-area/" + area
+
+
+	if value,ok := CACHE.Get(FullPath); ok{
+
+		var LocationAreaInfo LocationAreaData
+		Decoder := json.NewDecoder(bytes.NewReader(value))
+		err = Decoder.Decode(&LocationAreaInfo)
+		if err != nil{
+			return []PokemonEncounter{}, fmt.Errorf("error Decoding Json: %v", err)
+		}
+		return LocationAreaInfo.PokemonEncounters, nil
+
+		
+	}
+
+
+	res, err := http.Get(FullPath)
+	if err != nil{
+		return []PokemonEncounter{}, err
+	}
+	if res.StatusCode != http.StatusOK{
+		return []PokemonEncounter{}, fmt.Errorf("bad Request: %v", res.StatusCode)
+	}
+
+	defer res.Body.Close()
+	cacheValue, err := io.ReadAll(res.Body)
+    if err != nil {
+        return []PokemonEncounter{}, fmt.Errorf("error reading response body: %v", err)
+    }
+	
+	var LocationAreaInfo LocationAreaData
+	Decoder := json.NewDecoder(bytes.NewReader(cacheValue))
+	err = Decoder.Decode(&LocationAreaInfo)
+	if err != nil{
+		return []PokemonEncounter{}, fmt.Errorf("error Decoding Json: %v", err)
+	}
+	CACHE.Add(FullPath,cacheValue)
+	return LocationAreaInfo.PokemonEncounters, nil
+
 }
--- main.go
+++ main.go
@@ -3,98 +3,119 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"github.com/EchidnaTheG/PokeDex/internal"
 	"os"
 	"strings"
-	"github.com/EchidnaTheG/PokeDex/internal"
-
 )
 
-//declaring CACHE type early
+// declaring CACHE type early
 var CACHE *internal.Cache
 
 // General Struct For All CLI Commands In The App
-type CliCommand struct{
-	name string
+type CliCommand struct {
+	name        string
 	description string
-	callback func() error
+	callback    func(param string) error
 }
 
 // Initiliazing The Map That Contains All The Commands, the map is indexed using the names of the commands and the values are struct type CliCommand for that name
-var SupportedCommands map[string]CliCommand =  make(map[string]CliCommand)
-
-
+var SupportedCommands map[string]CliCommand = make(map[string]CliCommand)
 
 // The Help CliCommand that is used by the CliCommand help in the SupportedCommands map as a callback method
-func Help() error {
-		fmt.Println("Welcome to the Pokedex!")
-		fmt.Println("Usage:")
-		fmt.Println("\nhelp: Displays a help message")
-		fmt.Println("exit: Exit the Pokedex")
-		return nil
-	}
+func Help(_ string) error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println("\nhelp: Displays a help message")
+	fmt.Println("exit: Exit the Pokedex")
+	return nil
+}
 
-// The exit CliCommand that is used by the CliCommand exit in the SupportedCommands map as a callback method	
-func Exit() error{
+// The exit CliCommand that is used by the CliCommand exit in the SupportedCommands map as a callback method
+func Exit(_ string) error {
 	fmt.Println("Closing the Pokedex... Goodbye!")
 	defer os.Exit(0)
 	return nil
 }
-// The map CliCommand that is used by the CliCommand map in the SupportedCommands map as a callback method, it used internal functions to manage the Api calls, not yet finalized	
-func Map() error{
-	Locations, err := internal.GetLocationData(&config,false,CACHE)
-	if err != nil{
-		fmt.Printf("An Exception Has Ocurred: %v\n",err)
+
+// The map CliCommand that is used by the CliCommand map in the SupportedCommands map as a callback method, it used internal functions to manage the Api calls, not yet finalized
+func Map(_ string) error {
+	Locations, err := internal.GetLocationData(&config, false, CACHE)
+	if err != nil {
+		fmt.Printf("An Exception Has Ocurred: %v\n", err)
 	}
-	for _, Location := range Locations{
+	for _, Location := range Locations {
 		fmt.Println(Location.Name)
-	
+
 	}
 	return nil
 }
 
-func Mapb() error{
-	Locations, err := internal.GetLocationData(&config,true,CACHE)
-	if err != nil{
-		fmt.Printf("An Exception Has Ocurred: %v\n",err)
+func Mapb(_ string) error {
+
+	Locations, err := internal.GetLocationData(&config, true, CACHE)
+	if err != nil {
+		fmt.Printf("An Exception Has Ocurred: %v\n", err)
 	}
-	for _, Location := range Locations{
+	for _, Location := range Locations {
 		fmt.Println(Location.Name)
-	
+
 	}
 	return nil
 }
-var config= internal.Config{Next:nil,Previous:nil}
+
+func Explore(area string) error {
+	fmt.Printf("Exploring %v...\n", area)
+	listOfPokemon, err := internal.GetPokemonInArea(area,CACHE)
+	if err != nil{
+		return err
+	}
+	fmt.Printf("Found Pokemon:\n")
+	for _, data := range listOfPokemon{
+		fmt.Printf(" - %v\n",data.Pokemon.Name)
+	}
+	
+	return nil
+}
+
+var config = internal.Config{Next: nil, Previous: nil}
+
 //Initializing the SupportedCommands Map
 
-func init(){
+func init() {
 	CACHE = internal.NewCache(internal.INTERVAL)
 
-	SupportedCommands=  make(map[string]CliCommand)
-	SupportedCommands["help"] =CliCommand{
-	name: "help",
-	description: "Gives Help about app",
-	callback: Help,
+	SupportedCommands = make(map[string]CliCommand)
+	SupportedCommands["help"] = CliCommand{
+		name:        "help",
+		description: "Gives Help about app",
+		callback:    Help,
 	}
-	SupportedCommands["exit"]=CliCommand{
-		name: "exit",
-		description:"exits the program",
-		callback: Exit,
+	SupportedCommands["exit"] = CliCommand{
+		name:        "exit",
+		description: "exits the program",
+		callback:    Exit,
 	}
-	SupportedCommands["map"]= CliCommand{
-		name:"map",
+	SupportedCommands["map"] = CliCommand{
+		name:        "map",
 		description: "lists all the locations in batches of 20",
-		callback: Map,
+		callback:    Map,
 	}
-	SupportedCommands["mapb"]= CliCommand{
-		name:"mapb",
+	SupportedCommands["mapb"] = CliCommand{
+		name:        "mapb",
 		description: "lists all the locations in batches of 20, goes back 1 batch",
-		callback: Mapb,
+		callback:    Mapb,
 	}
-
-	
-} 
-
-
+	SupportedCommands["mapb"] = CliCommand{
+		name:        "mapb",
+		description: "lists all the locations in batches of 20, goes back 1 batch",
+		callback:    Mapb,
+	}
+	SupportedCommands["explore"] = CliCommand{
+		name:        "explore",
+		description: "lists all the pokemon in a specific area",
+		callback:    Explore,
+	}
+}
 
 // Helper function for capturing user input
 func cleanInput(text string) []string {
@@ -102,26 +123,39 @@ func cleanInput(text string) []string {
 
 }
 
-
 // Main function and app loop. Scanner is initialized for input and a infinite loop starts, user input captured and text outputted, as well as commands called
 func main() {
 	Scanner := bufio.NewScanner(os.Stdin)
-	
+
 	for {
 		fmt.Print("Pokedex > ")
 		Scanner.Scan()
 		userInputs := cleanInput(strings.ToLower(Scanner.Text()))
-		
-		if len(userInputs) > 0{
-			_,ok := SupportedCommands[userInputs[0]]; if !ok{
+
+		if len(userInputs) > 0 {
+			firstValue := userInputs[0]
+			_, ok := SupportedCommands[firstValue]
+			if !ok {
 				println("Unknown Command")
 				continue
 			}
-			err := SupportedCommands[userInputs[0]].callback()
-			if err != nil{
-				fmt.Printf("Error!: %v\n", err)
+			switch firstValue {
+			case "explore":
+				if len(userInputs) <= 1{
+					fmt.Println("Not Enough Flags Given")
+					continue
+				}
+				err := SupportedCommands[firstValue].callback(userInputs[1])
+				if err != nil {
+					fmt.Printf("Error!: %v\n", err)
+				}
+			default:
+				err := SupportedCommands[userInputs[0]].callback("")
+				if err != nil {
+					fmt.Printf("Error!: %v\n", err)
+				}
 			}
+
 		}
 	}
 }
-
